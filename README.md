2ecc2f765b489123c0cf6bb1754ea03bf2992ce7
Nathan Trujillo <nathan.trujillo@intensity.com>
2016-11-16 22:42:24 -0800

    Use elapsedMicros for RPM detection.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # pov-motor-scaler
+
+
+Cheap and dirty PID-like motor control using stats from a hall effect sensor
--- pov-motor-scaler.ino
+++ pov-motor-scaler.ino
@@ -1,25 +1,26 @@
 /*
-https://github.com/FastLED/FastLED/wiki/SPI-Hardware-or-Bit-banging
-Teensy 3/3.1:
+https://github.com/FastLED/FastLED/wiki/SPI-Hardware-or-Bit-banging Teensy 3/3.1:
 Hardware SPI - data 11, clock 13
 Hardware SPI - data 7, clock 14
 */
 
+#define TEENSY_SYS 3.3
+#define ARDUINO_SYS 5
 //Define Variables we'll be connecting to
 double gap;
+
+elapsedMicros timeSinceMagnet;
 
 //Define the aggressive and conservative Tuning Parameters
   
 int power = 0;
-const int maxPower = 130;
+const int maxPower = 140;
 const int motorInit = 101;
 
-float maxVoltage = 2.4;
+float maxVoltage = 2.8;
 float voltage = 0.0;
 
 signed int ascending = 1;
-
-const int avgFactor = 16;
 
 boolean cango = false;
 
@@ -31,57 +32,45 @@ const unsigned int ACCELFACTOR = 1;
 
 const float RCDIFF = 0.128;
 
-const float SYSTEMVOLTAGE = 3.3;// TEENSY 3.3v ARDUINO 5v
+const float SYSTEMVOLTAGE = TEENSY_SYS;// TEENSY 3.3v ARDUINO 5v
 
+const int avgFactor = 16;
 float avg = 0.1;
-volatile int rpmcount = 0;
-volatile float rpm = 0.1;
+volatile float rpm = 0.0;
 
 const int smoothCyclesLimit = 3;
 int smoothCyclesIterator = 0;
 
-volatile int current = 0;
-volatile int last = 0;
-
-float now = 0.0;
-int innerNow = 0;
-int lastmillis = 0;
-
-
 // http://www.massmind.org/Techref/io/sensor/interface.htm
-void average(int sample) // sample is in ms
+void average(float sample) // sample is in ms
 { 
-  //avg -= (avg>>4)
+  //avg -= (avg>>4) /// can only shift int, so use avgfactor
   avg -= (avg / avgFactor);   // output result is 1/16th of accumulator   // subtract l/16th of the accumulator
   avg += sample;     // add in the new sample
 }
 
 void setupHallSensor()
 {
+  pinMode(HALLPIN, INPUT);
   digitalWrite(HALLPIN, HIGH);
-  delay(1000);
+  delay(2000);
   Serial.println("Hall Setup.");  
-  delay(3000);
-  attachInterrupt(HALLPIN, interruptHandler, FALLING);
+  delay(2000);
+  attachInterrupt(HALLPIN, interruptHandler, RISING);
   Serial.println("Go!");
-  rpmcount = 0.0;
-  rpm = 0.0;
 }
 
 void interruptHandler()
-{ 
-    rpmcount++;
-    current = now - last;
-    average(current);
-    last = millis();
-    //Serial.println("TICK!!!");
+{   
+    rpm = rpmFromMicros();    
+    timeSinceMagnet = 0;
 }
 
 
 void printPowerStatus()
 {
-    //Serial.print(" Power: ");    
-    //Serial.print(power);
+//    Serial.print(" Power: ");    
+//    Serial.print(power);
 
 //    Serial.print(" Voltage: ");    
 //    Serial.print(voltage);
@@ -92,25 +81,22 @@ void printPowerStatus()
 
 void printRpmStatus()
 {
-    //Serial.print(" gap: " );
-    //Serial.print(gap);
     
-    //Serial.print(" AVG RPM: ");    
-    Serial.println(valueToRpm(avg) * 16);
-    
-    //Serial.print(" Hz: ");    
-    //Serial.print(rpmcount);
+    Serial.print(" AVG RPM: "); 
+    Serial.print(avg);   
 
-    //Serial.print(" RPM: ");
-    //Serial.println(rpm);
+    Serial.print(" HZ: ");
+    Serial.print(rpm/60);
     
+    Serial.print(" RPM: ");
+    Serial.println(rpm);
+        
 }
 
 void setup() {
   Serial.begin(9600);
   pinMode(DRIVEPIN, OUTPUT);
   analogWrite(DRIVEPIN, 0);
-  pinMode(HALLPIN, INPUT);
   setupHallSensor();
   power = motorInit;
   Serial.println(power);
@@ -118,7 +104,6 @@ void setup() {
 }
 
 void loop() {
-  now = millis();
 //  if(cango 
 //  && (now - lastmillis) > 100) {
 //    if(gap < (TARGETRPM / 20)) {
@@ -128,17 +113,12 @@ void loop() {
 //      accelerate();
 //    }    
 //  }
-  if (now - lastmillis > 1000) { 
-    cango = true;
-    lastmillis = now;      
-    rpm = valueToRpm(current);
-    current = 0;
+  if (timeSinceMagnet >= 1000000) { // 1 sec intervals
+    average(rpm); 
+    cango = true;    
     voltage = powerToVolts(power);
-    //Serial.println(rpm);
     printPowerStatus();
-    printRpmStatus();
-    rpmcount = 0;
-          
+    printRpmStatus();          
     gap = TARGETRPM - rpm; //distance away from TARGETRPM    
 
     if(smoothCyclesLimit == smoothCyclesIterator) {
@@ -156,40 +136,31 @@ void loop() {
   }
 }
 
-float valueToRpm(float value)
-{
-  if(value == 0) {
-    return 0;
-  }
-  return (60.0 / value) * 1000.0;// (60/B2)*1000
-}
-
-void accelerate()
-{
+void accelerate() {
   ascending = 1;
   signalMotor();
 }
 
-void decelerate()
-{
+void decelerate() {
   ascending = -1;
   signalMotor();  
 }
 
-void signalMotor()
-{
+void signalMotor() {
   power += (ascending * ACCELFACTOR);
   checkRange();
   analogWrite(DRIVEPIN, (int) power);
 }
 
-void checkRange()
-{    
+void checkRange() {    
     power = constrain(power, motorInit, maxPower);
     voltage = constrain(voltage, 0, maxVoltage);
 }
 
-float powerToVolts(float p)
-{
+float powerToVolts(float p) {
   return (p/255) * SYSTEMVOLTAGE + RCDIFF;// p >> 8
+}
+
+float rpmFromMicros(void) {
+  return (1000000 / timeSinceMagnet) / 60;
 }
